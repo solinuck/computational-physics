e2f7a82e097389cdb6940612988397fb67f9551d
Erwin Rudi <erwin.rudi@gmail.com>
2020-04-10 12:58:25 +0200

    generalized, so that arbitrary function and boundary points can be set

--- exercise1/simpson_2.py
+++ exercise1/simpson_2.py
@@ -9,19 +9,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def is_even(array):
-    """Check whether a given array is a numpy array and check if the length is even."""
-    assert type(array) == np.ndarray, "Not a valid numpy array"
-    if array.size % 2 == 0:
-        return True
-    else:
-        return False
+def simpson(a, b, N, func):
+    x, h = np.linspace(a, b, N, retstep=True)
+    function_values = func(x)
+    integral = simpson_rule(h, function_values)
+    return integral, h
 
 
-def simpson_rule(function_values, h):
+def simpson_rule(h, function_values):
     """Simpson rule for numerical integration.
     Arguments: function_values (numpy array)
     """
+
     if is_even(function_values):
         # even
         first_term = (
@@ -29,7 +28,7 @@ def simpson_rule(function_values, h):
             * (5 * function_values[-1] + 8 * function_values[-2] - function_values[-3])
             / 12
         )
-        rest = simpson_rule(function_values[:-1:], h)  # recursion, bitches
+        rest = simpson_rule(h, function_values[:-1:])  # recursion, bitches
         return first_term + rest
     else:
         # odd
@@ -46,25 +45,33 @@ def simpson_rule(function_values, h):
         return result
 
 
+def is_even(array):
+    """Check whether a given array is a numpy array and check if the length is even."""
+    assert type(array) == np.ndarray, "Not a valid numpy array"
+    if array.size % 2 == 0:
+        return True
+    else:
+        return False
+
+
 def theoretical_error(a, b, h, f):
     return (h ** 4) * f * (b - a) / 180
 
 
-x, h = np.linspace(0, np.pi / 2, 100, retstep=True)
-
-analytic = 1
-sin = np.sin(x)
-
 data = []
 hs = []
 es = []
-h_range = 10000
-for i in range(1, h_range):
-    x, h = np.linspace(0, np.pi / 2, 10 * i, retstep=True)
-    sin = np.sin(x)
-    r = simpson_rule(sin, h)
+
+a = 0
+b = np.pi / 2
+function = np.sin
+analytic = 1
+
+h_range = 50000
+for i in range(3, h_range):
+    simp_result, h = simpson(a, b, i, np.sin)
     e = theoretical_error(0, np.pi / 2, h, 1)
-    data.append(r - 1)
+    data.append(np.abs(simp_result - analytic))
     hs.append(h)
     es.append(e)
     if i % 1000 == 0:
@@ -74,3 +81,6 @@ for i in range(1, h_range):
 plt.loglog(hs, data, label="Difference analytic")
 plt.loglog(hs, es, label="Theoretical Error")
 plt.legend()
+plt.xlabel("h")
+plt.ylabel("difference")
+plt.savefig("simpson_2.png")
