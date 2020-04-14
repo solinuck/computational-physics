c8ab94f316befd461e0ebfc90f81cbcb1e9d4f75
singulay <39281326+singulay@users.noreply.github.com>
2020-04-14 10:57:40 +0200

    added more comments to simpson

--- exercise1/simpson_2.py
+++ exercise1/simpson_2.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Apr  8 21:04:13 2020
-
-@author: Max
-"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def simpson(a, b, N, func):
+    """
+    Simpson integration.
+    Arguments:
+    a, b: Integration interval
+    N: Number of sampled points
+    func: function to integrate
+
+    """
     x, h = np.linspace(a, b, N, retstep=True)
-    function_values = func(x)
+    function_values = func(x) # generate an array of function values
     integral = simpson_rule(h, function_values)
     return integral, h
 
@@ -22,22 +25,22 @@ def simpson_rule(h, function_values):
     """
 
     if is_even(function_values):
-        # even
+        # even length
         first_term = (
             h
             * (5 * function_values[-1] + 8 * function_values[-2] - function_values[-3])
             / 12
         )
-        rest = simpson_rule(h, function_values[:-1:])  # recursion, bitches
+        rest = simpson_rule(h, function_values[:-1:])  # recursive calculation of the rest of the array
         return first_term + rest
     else:
-        # odd
+        # odd length
         result = (
             h
             * (
                 function_values[0]
                 + function_values[-1]
-                + np.sum(4 * function_values[1:-1:2])
+                + np.sum(4 * function_values[1:-1:2]) # formula from the script with some fancy numpy indexing
                 + np.sum(2 * function_values[2:-2:2])
             )
             / 3
@@ -54,7 +57,7 @@ def is_even(array):
         return False
 
 
-def theoretical_error(a, b, h, f):
+def theoretical_error(a, b, h, f): # formula from the script, we assumed zeta to produce the maximal value.
     return (h ** 4) * f * (b - a) / 180
 
 
@@ -69,11 +72,11 @@ analytic = 1
 
 h_range = 10000
 for i in range(3, h_range):
-    simp_result, h = simpson(a, b, 5 * i, function)
+    simp_result, h = simpson(a, b, 5 * i, function) # iterate over different slicings
     e = theoretical_error(0, np.pi / 2, h, 1)
     data.append(np.abs(simp_result - analytic))
     hs.append(h)
-    es.append(e)
+    es.append(e) # save data to arrays for plotting
     if i % 1000 == 0:
         perc = i / h_range * 100
         print(f"{perc}%")
