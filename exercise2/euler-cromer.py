b47c954b6c8dc204cd3e1255c811a8affdfb3c3a
Erwin Rudi <erwin.rudi@gmail.com>
2020-04-16 11:11:53 +0200

    plotting + fix error in verlet method

--- exercise2/euler-cromer.py
+++ exercise2/euler-cromer.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def harmonic_euler(time_steps, tau, x0, v0 = 0, k = 1, m = 1):
+
+def harmonic_euler(time_steps, tau, x0, v0=0, k=1, m=1):
     x = np.zeros(time_steps)
     x[0] = x0
     v = np.zeros(time_steps)
@@ -10,34 +11,36 @@ def harmonic_euler(time_steps, tau, x0, v0 = 0, k = 1, m = 1):
 
     E = np.zeros(time_steps)
 
-    for i in range(0, time_steps-1):
-        a[i] = -k/m * x[i]
-        v[i+1] = v[i] + tau*a[i]
-        x[i+1] = x[i] + tau*v[i]
-        E[i] = 0.5*m*v[i]**2  + 0.5*k*x[i]**2
+    for i in range(0, time_steps - 1):
+        a[i] = -k / m * x[i]
+        v[i + 1] = v[i] + tau * a[i]
+        x[i + 1] = x[i] + tau * v[i]
+        E[i] = 0.5 * m * v[i] ** 2 + 0.5 * k * x[i] ** 2
+    E[-1] = E[-2] * (1 + k / m * tau ** 2)
     return x, E
 
 
-def harmonic_verlet(time_steps, tau, x0, v0 = 0, k = 1, m = 1):
+def harmonic_verlet(time_steps, tau, x0, v0=0, k=1, m=1):
     x = np.zeros(time_steps)
-    a0 = -k/m * x0
-    x[0] = x0 - tau*v0 + (tau**2) * a0 / 2
+    a0 = -k / m * x0
+    x[0] = x0 - tau * v0 + (tau ** 2) * a0 / 2
     x[1] = x0
     v = np.zeros(time_steps)
-    v[0] = v0
+    # v[1] = v0
 
-    E = np.zeros(time_steps-1)
-    E[0] = 0.5 * (m * v[0]**2 + k * x[0]**2)
+    E = np.zeros(time_steps)
+    # E[1] = 0.5 * (m * v[1] ** 2 + k * x[1] ** 2)
     a = np.zeros(time_steps)
-    for i in range(1, time_steps-1):
-        a[i] = -k/m * x[i]
-        x[i+1] = 2 * x[i] - x[i-1] + tau**2 * a[i]
-        v[i] = (x[i+1] - x[i-1]) / (2 * tau)
-        E[i] =  0.5 * (m * v[i]**2 + k * x[i]**2)
+    for i in range(1, time_steps - 1):
+        a[i] = -k / m * x[i]
+        x[i + 1] = 2 * x[i] - x[i - 1] + tau ** 2 * a[i]
+        v[i] = (x[i + 1] - x[i - 1]) / (2 * tau)
+        E[i] = 0.5 * (m * v[i] ** 2 + k * x[i] ** 2)
 
-    return x, E
+    return x[1:-1], E[1:-1]
 
-def euler_cromer(time_steps, tau, x0, v0 = 0, k = 1, m = 1):
+
+def euler_cromer(time_steps, tau, x0, v0=0, k=1, m=1):
     x = np.zeros(time_steps)
     x[0] = x0
     v = np.zeros(time_steps)
@@ -46,22 +49,32 @@ def euler_cromer(time_steps, tau, x0, v0 = 0, k = 1, m = 1):
 
     E = np.zeros(time_steps)
 
-    for i in range(0, time_steps-1):
-        a[i] = -k/m * x[i]
-        v[i+1] = v[i] + tau*a[i]
-        x[i+1] = x[i] + tau*v[i+1]
-        E[i] = 0.5 * (m * v[i]**2 + k * x[i]**2)
-    E[-1] = 0.5 * (m * v[-1]**2  + k * x[-1]**2)
+    for i in range(0, time_steps - 1):
+        a[i] = -k / m * x[i]
+        v[i + 1] = v[i] + tau * a[i]
+        x[i + 1] = x[i] + tau * v[i + 1]
+        E[i] = 0.5 * (m * v[i] ** 2 + k * x[i] ** 2)
+    E[-1] = 0.5 * (m * v[-1] ** 2 + k * x[-1] ** 2)
     return x, E
 
-x, E = euler_cromer(1000, 0.1, 10)
-plt.plot(x, label="amplitude")
-plt.plot(E, label="total energy")
-plt.legend()
-plt.show()
 
-'''
-data = harmonic_verlet(1000, 0.1, 10)
-plt.plot(data)
-plt.show()
-'''
+def plotting(x, E, color, labelName):
+    plt.plot(x, color=color, label=labelName)
+    plt.plot(E, color=color)
+    plt.legend()
+
+
+# data = harmonic_verlet(1000, 0.1, 10)
+
+x1, E1 = harmonic_euler(100, 0.1, 10)
+x2, E2 = harmonic_verlet(100, 0.1, 10)
+x3, E3 = euler_cromer(100, 0.1, 10)
+
+plotting(x1, E1, "green", "euler")
+plotting(x2, E2, "blue", "verlet")
+plotting(x3, E3, "red", "euler_cromer")
+
+plt.text(50, 60, "Energy")
+plt.text(20, 10, "Amplitude")
+plt.savefig("ex1_all.png")
+plt.close()
