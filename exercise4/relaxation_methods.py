c543cf25140862c1015a7ebb2a6465de01149207
Erwin Rudi <erwin.rudi@gmail.com>
2020-05-01 15:36:55 +0200

    task1: added gauss_seidel method

--- exercise4/relaxation_methods.py
+++ exercise4/relaxation_methods.py
@@ -31,6 +31,32 @@ def jacobi(grid, threshold):
     return new_grid, phi_100, iter
 
 
+def gauss_seidel(grid, threshold):
+    nx = grid.shape[0]
+    ny = grid.shape[1]
+
+    new_grid = fill_boundary(grid)
+    old_grid = new_grid.copy()
+
+    iter = 0
+    while np.linalg.norm(new_grid - old_grid) > threshold or iter == 0:
+        iter += 1
+        old_grid = new_grid.copy()
+
+        # loop over all inner points
+        for i in range(1, nx - 1):
+            for j in range(1, ny - 1):
+                new_grid[i, j] = (
+                    new_grid[i + 1, j]
+                    + new_grid[i - 1, j]
+                    + new_grid[i, j + 1]
+                    + new_grid[i, j - 1]
+                ) / 4
+        if iter == 100:
+            phi_100 = new_grid.copy()
+    return new_grid, phi_100, iter
+
+
 def fill_boundary(grid):
     nx = grid.shape[0]
     ny = grid.shape[1]
@@ -45,31 +71,37 @@ def fill_boundary(grid):
     return grid
 
 
+def plot3D(phi, savename, nx=81, ny=81):
+    allX = np.linspace(-np.pi / 2, np.pi / 2, nx)
+    allY = np.linspace(-np.pi / 2, np.pi / 2, ny)
+
+    xx, yy = np.meshgrid(allX, allY)
+
+    fig = plt.figure()
+    ax = fig.gca(projection="3d")
+
+    surf = ax.plot_surface(
+        xx, yy, phi, cmap=cm.coolwarm, linewidth=0, antialiased=False
+    )
+
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+    plt.savefig("{}.png".format(savename))
+    plt.close()
+
+
 nx = 81
 ny = 81
 eps = 10e-5
 
 grid = np.zeros((nx, ny))
 
-phi_final, phi_100, iter = jacobi(grid, eps)
+jacobi_final, jacobi_100, iter_jacobi = jacobi(grid, eps)
+# seidel_final, seidel_100, iter_seidel = gauss_seidel(grid, eps)
 
-allX = np.linspace(-np.pi / 2, np.pi / 2, nx)
-allY = np.linspace(-np.pi / 2, np.pi / 2, ny)
-
-xx, yy = np.meshgrid(allX, allY)
-
-fig = plt.figure()
-ax = fig.gca(projection="3d")
-
-surf = ax.plot_surface(
-    xx, yy, phi_100, cmap=cm.coolwarm, linewidth=0, antialiased=False
-)
-
-fig.colorbar(surf, shrink=0.5, aspect=5)
-
-plt.savefig("jacobi.png")
-
-print(iter)
+plot3D(jacobi_100, "jacobi_100")
+plot3D(jacobi_final, "jacobi_final")
+# plot3D(seidel_100, "seidel_100")
+# plot3D(seidel_final, "seidel_final")
 
 from IPython import embed
 
