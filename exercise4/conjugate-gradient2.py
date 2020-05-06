ab77b6a9da9337c324a8804d15da735f513b0414
singulay <39281326+singulay@users.noreply.github.com>
2020-05-06 18:32:20 +0200

    added comments

--- exercise4/conjugate-gradient2.py
+++ exercise4/conjugate-gradient2.py
@@ -6,21 +6,20 @@ from matplotlib import cm
 def cg_method(b, x, epsilon):
     n = -multA(x) + b
     g = n
-    grad = np.inf
+    grad = np.inf  # make sure loop executes at least once
     iter = 0
     while grad >= epsilon:
         iter += 1
 
-        # l = g @ g
-        l = (g @ g) / (n @ (multA(n)))
+        lam = (g @ g) / (n @ (multA(n)))  # formula from lecture
 
-        x += l * n
-        g_1 = g - l * multA(n)
+        x += lam * n  # move towards minimum
+        g_1 = g - lam * multA(n)  # update g and n
         n_1 = g_1 + n * (g_1 @ g_1) / (g @ g)
 
         g = g_1
         n = n_1
-        grad = np.linalg.norm(n)
+        grad = np.linalg.norm(n)  # get magnitude of gradient
 
         if iter == 10:
             x_10 = x.copy()
@@ -31,13 +30,12 @@ def cg_method(b, x, epsilon):
     return x, x_10, x_50, x_100, iter
 
 
-
 def multA(x):
     res = np.empty_like(x)  # result vector
     width = int(np.sqrt(x.size))
-    res = 4 * x  # 4 * identity
-    for i in range(x.size):  # rest
-        if i >= width:  # not at left edge
+    res = 4 * x  # 4 * identity, ie the diagonal elements of A
+    for i in range(x.size):  # off diagonal elements
+        if i >= width:  # point i of grid not at left edge -> has a left neighbor
             res[i] -= x[i - width]
         if i < (width - 1) * width:  # not at right edge
             res[i] -= x[i + width]
@@ -49,18 +47,18 @@ def multA(x):
     return res
 
 
-def create_boundary(size):
-<<<<<<< HEAD
-    res = np.zeros(size)  #
-=======
+def create_boundary(size):  # create b vector
+
     res = np.zeros(size)
->>>>>>> 5d1f8da48a76f55933bf352b02936bddfff1b661
+
     width = int(np.sqrt(size))
     allX = np.linspace(-np.pi / 2, np.pi / 2, width)
     side = np.cos(allX)
+    # since all edges have the same boundary conditions,
+    # we use only one array for all four
     for i in range(size):
-<<<<<<< HEAD
-        if i < width:  # at left edge
+
+        if i < width:  # point at left edge -> add boundary condition
             res[i] += side[i % width]
         if i >= (width - 1) * width:  # at right edge
             res[i] += side[i % width]
@@ -70,24 +68,11 @@ def create_boundary(size):
             res[i] += side[(i + 1) // width - 1]
     return res
 
-=======
-        j = i % width
-        if i < width:  # at left edge
-            res[i] += side[j]
-        if i >= (width - 1) * width:  # at right edge
-            res[i] += side[j]
-        if i % width == 0:  # at bottom edge
-            res[i] += side[j]
-        if (i + 1) % width == 0:  # at top edge
-            res[i] += side[j]
-    return res
->>>>>>> 5d1f8da48a76f55933bf352b02936bddfff1b661
 
 def plot3D(phi, savename, nx=81, ny=81):
     allX = np.linspace(-np.pi / 2, np.pi / 2, nx)
     allY = np.linspace(-np.pi / 2, np.pi / 2, ny)
 
-<<<<<<< HEAD
     xx, yy = np.meshgrid(allX, allY)
 
     fig = plt.figure()
@@ -103,10 +88,10 @@ def plot3D(phi, savename, nx=81, ny=81):
 
 
 x = np.zeros((79 ** 2))
-x.fill(1)
-b = create_boundary(79 ** 2)
+x.fill(1)  # first guess
+b = create_boundary(79 ** 2)  # boundaries
 
-cg_final, cg_10, cg_50, cg_100, iter = cg_method(b, x, 1e-5)
+cg_final, cg_10, cg_50, cg_100, iter = cg_method(b, x, 1e-5)  # solve
 
 cg_final = cg_final.reshape(79, 79)
 cg_10 = cg_10.reshape(79, 79)
@@ -120,14 +105,3 @@ plot3D(cg_final, "img/conjugate", 79, 79)
 plot3D(cg_10, "img/conjugate_10", 79, 79)
 plot3D(cg_50, "img/conjugate_50", 79, 79)
 plot3D(cg_100, "img/conjugate_100", 79, 79)
-=======
-# print(multA(np.array([0, 1, 2, 3, 4, 5, 6, 7, 8])))
-
-x = np.zeros((79 ** 2))
-x.fill(1)
-b = create_boundary(79 ** 2)
-
-result = cg_method(b, x, 1e-5)
-
-plot3D(result, "conjugate", 79, 79)
->>>>>>> 5d1f8da48a76f55933bf352b02936bddfff1b661
