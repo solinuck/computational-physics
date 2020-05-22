52df5b3b04d8aecc6d8314c8104d3caf9fc04e7f
Erwin Rudi <erwin.rudi@gmail.com>
2020-05-22 15:39:25 +0200

    add logging and 3D generalization

--- exercise5/main.py
+++ exercise5/main.py
@@ -1,4 +1,58 @@
+import sys
+
+from pathlib import Path
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
 from mdengine import MDEngine
 
+
+class lennardJones:
+    def __init__(self, eps, sig):
+        self.eps = eps
+        self.sig = sig
+
+    def force(self, r):
+        return 24 * self.eps / r * (2 * (self.sig / r) ** 12 - (self.sig / r) ** 6)
+
+    def pot(self, r):
+        return 4 * self.eps * ((self.sig / r) ** 12 - (self.sig / r) ** 6)
+
+
+class Logging:
+    def __init__(self):
+        pass
+
+    def get_console_handler(self, formatter):
+        console_handler = logging.StreamHandler(sys.stdout)
+        console_handler.setFormatter(formatter)
+        return console_handler
+
+    def get_file_handler(self, log_file, formatter):
+        file_handler = TimedRotatingFileHandler(self.log_file, when="midnight")
+        file_handler.setFormatter(formatter)
+        return file_handler
+
+    def get_logger(self, logger_name, log_file, formatter):
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(logging.DEBUG)
+        logger.addHandler(self.get_console_handler(formatter))
+        from IPython import embed
+
+        embed()
+        Path(f"/logs/{log_file}").mkdir(parents=True, exist_ok=True)
+        logger.addHandler(self.get_file_handler(log_file, formatter))
+        logger.propagate = False
+        return logger
+
+
 if __name__ == "__main__":
-    MDEngine()
+    logs = Logging()
+    lj = lennardJones(eps=1.65e-21, sig=3.4e-10)
+    engine = MDEngine(d=2, n=5, m=1, l=2, tau=1, potential=lj)
+    engine.initialize()
+    engine.equilibrate()
+
+    energy_formater = f"{engine.temp} - {engine.ekin} - {engine.epot} - {engine.etot}"
+    my_logger = logs.get_logger("eq_engery", "equilibration.e", energy_formater)
+    my_logger.info("")
--- exercise5/mdengine.py
+++ exercise5/mdengine.py
@@ -1,5 +1,4 @@
 import numpy as np
-from scipy.spatial.distance import cdist, pdist
 import itertools
 
 
@@ -9,32 +8,39 @@ class MDEngine:
     k_angle = 1
     k_dihedral = 1
 
-    def __init__(self, d=2, n=5, m=1, l=2, tau=1):
+    def __init__(self, d, n, m, l, tau, potential):
         self.d = d
         self.n = n
         self.m = m
         self.l = l
         self.tau = tau
+        self.pot = potential
         self.epot = 0
         self.ekin = 0
         self.etot = 0
         self.temp = 0
-        self.initialize()
-        self.update()
 
     def initialize(self):
         self.initR()
         self.initV()
         self.correctV()
-        self.updateFirstTime()
+        self.update(init=True)
 
     def initR(self):
-        self.r = np.random.uniform(-self.l / 2, self.l / 2, (self.n, self.d))
+        self.r = np.random.uniform(-self.l / 2, self.l / 2, (self.n, 3))
+        if self.d == 1:
+            self.r[:, 1] = 0
+        if self.d <= 2:
+            self.r[:, 2] = 0
 
     def initV(self):
         temp = 1
         sig = np.sqrt(self.m / (self.kb * temp))
-        self.v = np.random.normal(loc=0, scale=sig, size=(self.n, self.d))
+        self.v = np.random.normal(loc=0, scale=sig, size=(self.n, 3))
+        if self.d == 1:
+            self.v[:, 1] = 0
+        if self.d <= 2:
+            self.v[:, 2] = 0
 
     def correctV(self):
         if np.any(np.around(self.totMomentum(), 15)):
@@ -43,36 +49,30 @@ class MDEngine:
     def totMomentum(self):
         return np.sum(self.m * self.v, axis=0)
 
-    def updateFirstTime(self):
-        a = self.calcForce() / self.m
-        self.r += self.tau * self.v + self.tau ** 2 * a / 2
-        self.a_nplus1 = self.calcForce() / self.m
-        self.v += self.tau / 2 * (a + self.a_nplus1)
-        self.a = self.a_nplus1
-        self.ekin = np.sum(self.v[:, 0] ** 2 + self.v[:, 1] ** 2) * self.m / 2
-
-    # def equalibrate(self):
-    #     for t in np.linspace(0, 10 * 0.01, 10):
-    #         self.update()
-    #
-    #     pass
+    def equilibrate(self):
+        for t in np.linspace(0, 10 * 0.01, 10):
+            self.update()
+            self.storeFrame()
+        print("end")
 
     def storeFrame(self):
         pass
 
     def computeEpot(self):
+        self.epot = 0
         eAngle = 0
         eDihedral = 0
         for p1, p2, p3 in itertools.permutations(self.r, 3):
             eAngle += self.k_angle * self.angle_between(p1, p2, p3) ** 2
         for p1, p2, p3, p4 in itertools.permutations(self.r, 4):
-            eDihedral += self.k_dihedral * (1 + np.cos(self.dihedral(p1, p2, p3, p4)))
-        from IPython import embed; embed()
+            eDihedral += self.k_dihedral * (
+                1 + np.cos(self.dihedral(p1, p2, p3, p4))
+            )  # formula needs to be checked
         self.epot += eAngle + eDihedral
 
     def angle_between(self, a, b, c):
-        ba = (self.toroDist(b[0], a[0]), self.toroDist(b[1], a[1]))
-        bc = (self.toroDist(c[0], b[0]), self.toroDist(c[1], b[1]))
+        ba = self.toroDist3D(b, a)
+        bc = self.toroDist3D(c, b)
 
         ba_u = ba / np.linalg.norm(ba)
         bc_u = bc / np.linalg.norm(bc)
@@ -80,57 +80,61 @@ class MDEngine:
         return np.arccos(np.clip(np.dot(ba_u, bc_u), -1.0, 1.0))
 
     def dihedral(self, a, b, c, d):
-        v1 = (self.toroDist(b[0], a[0]), self.toroDist(b[1], a[1]))
-        v2 = (self.toroDist(c[0], b[0]), self.toroDist(c[1], b[1]))
-        v3 = (self.toroDist(d[0], c[0]), self.toroDist(d[1], c[1]))
-
+        v1 = self.toroDist3D(b, a)
+        v2 = self.toroDist3D(c, b)
+        v3 = self.toroDist3D(d, c)
 
         v2 /= np.linalg.norm(v2)
 
-        a = v1 - np.dot(v1, v2)*v2
-        b = v3 - np.dot(v3, v2)*v2
+        a = v1 - np.dot(v1, v2) * v2
+        b = v3 - np.dot(v3, v2) * v2
 
         x = np.dot(a, b)
         y = np.dot(np.cross(v2, a), b)  # np.cross only works for 3D vector
-        from IPython import embed; embed()
         return np.arctan2(y, x)
 
-    def update(self):
+    def update(self, init=False):
         self.computeEpot()
+        if init:
+            self.a = self.calcForce() / self.m
         self.r += self.tau * self.v + self.tau ** 2 * self.a / 2
         self.a_nplus1 = self.calcForce() / self.m
         self.v += self.tau / 2 * (self.a + self.a_nplus1)
-        self.a = self.a_nplus1
-
-        from IPython import embed; embed()
+        self.a = self.a_nplus1.copy()
+        self.ekin = np.sum(self.v ** 2) * self.m / 2
+        self.temp = 2 * self.ekin / (3 * self.n * self.kb)
+        self.etot = self.ekin + self.epot
 
     def calcForce(self):
-        f = np.zeros((self.n, self.d))
+        f = np.zeros((self.n, 3))
         ePair = 0
         eNonBond = 0
         for i, (p1, p2) in enumerate(itertools.permutations(self.r, 2)):
-            dx = self.toroDist(p1[0], p2[0])
-            dy = self.toroDist(p1[1], p2[1])
-
             idx = i // (self.n - 1)
-            f[idx, 0] += self.ljForce(dx)
-            f[idx, 1] += self.ljForce(dy)
 
-            ePair += self.k_pair * (dx **2 + dy ** 2) / 2
+            dxyz = self.toroDist3D(p1, p2)
 
-            eNonBond += self.ljPot((dx ** 2 + dy ** 2) ** .5)
+            f[idx, 0] += self.pot.force(dxyz[0])
+            f[idx, 1] += self.pot.force(dxyz[1])
+            f[idx, 2] += self.pot.force(dxyz[2])
 
-        from IPython import embed; embed()
+            ePair += self.k_pair * (np.sum(dxyz ** 2)) / 2
+            eNonBond += self.pot.pot(np.sum(dxyz ** 2) ** 0.5)
+
+        f = np.nan_to_num(f)
         self.epot += ePair + eNonBond
         return f
 
-    def ljForce(self, r, eps=1.65e-21, sig=3.4e-10):
-        return 24 * eps / r * (2 * (sig / r) ** 12 - (sig / r) ** 6)
+    def toroDist3D(self, v1, v2):
+        return np.array(
+            (
+                self.toroDist1D(v2[0], v1[0]),
+                self.toroDist1D(v2[1], v1[1]),
+                self.toroDist1D(v2[2], v1[2]),
+            )
+        )
 
-    def ljPot(self, r, eps=1.65e-21, sig=3.4e-10):
-        return 4 * eps * ((sig / r) ** 12 - (sig / r) ** 6)
-
-    def toroDist(self, x1, x2):
+    def toroDist1D(self, x1, x2):  # Can we somehow parallelize this?
         x1 = self.getInitialcoordinates(x1)
         x2 = self.getInitialcoordinates(x2)
 
@@ -142,9 +146,6 @@ class MDEngine:
             dx = dx + self.l
 
         return dx
-
-    def euclidean(self, dx, dy):
-        return (dx ** 2 + dy ** 2) ** .5
 
     def getInitialcoordinates(self, x):
         while x > (self.l / 2):
